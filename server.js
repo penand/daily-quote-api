d70ed8e6636a61ab8a1155bb07bb022fdb311537
penand <marzone.ap@gmail.com>
2022-01-16 16:18:40 +0100

    fix: debug add ;

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middleware = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('data.json');
+
 const port = process.env.PORT || 3000;
 
-server.use(middleware)
-server.use(router)
-server.listen(port, () => {
-  console.log('JSON Server is running')
-})
+server.use(middleware);
+server.use(router);
+server.listen(port);
