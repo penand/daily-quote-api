07ebba7cc0488293dd1d85827847dce04f1a5993
penand <marzone.ap@gmail.com>
2022-01-16 16:23:19 +0100

    fix: add back middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json');
+const middleware = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
 
